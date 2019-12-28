46e947f86876a2bad01d0272453b327bbad36c0b
Paul <paul@paulgoesdeep.com>
2019-12-28 20:36:22 +0300

    Fix game spec

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,9 +1,13 @@
 RSpec.describe Game do
   it 'validates its structure' do
-    game = Game.new(id: '123abc', fleet: 'pirates', created_at: DateTime.now)
+    game = Game.new(id: '123abc',
+                    host_fleet: 'pirates',
+                    guest_fleet: 'spaniards',
+                    golden_ship: 0,
+                    created_at: DateTime.now)
 
     expect(game.id).to eq '123abc'
-    expect(game.fleet).to eq 'pirates'
+    expect(game.host_fleet).to eq 'pirates'
   end
 
   it 'does not allow wrong structure' do
